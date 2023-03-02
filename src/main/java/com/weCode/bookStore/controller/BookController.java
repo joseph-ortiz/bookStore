f7a9463b6264c29b288b9ad8fdc712cc4e36f679
Joseph Ortiz <josepho011235@gmail.com>
2023-03-02 14:59:44 -0500

    init

---
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,29 @@
+package com.weCode.bookStore.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.weCode.bookStore.dto.BookDTO;
+
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+    
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getBooks(){
+        BookDTO book = BookDTO.builder()
+            .title("my first book")
+            .build();
+        List<BookDTO> books = new ArrayList<>();
+        books.add(book);
+        return ResponseEntity.ok(books);
+        
+        
+    }
+}
---
+++ src/main/java/com/weCode/bookStore/dto/BookDTO.java
@@ -0,0 +1,24 @@
+package com.weCode.bookStore.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend.Prop;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDTO {
+    private String title;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
